6a5df8a32aa3adf2ca6da220882476fe92b81e93
Andrew Bunyea <abunyea@gmail.com>
2011-09-28 10:47:31 -0400

    Added string_of_move to Moves, doesn't place a gray marble. Move type should be changed to have six integer fields to account for gray marbles

--- source/Moves.ml
+++ source/Moves.ml
@@ -3,6 +3,10 @@ open Board
 type move = (int * int * int * int)
 type move_set = move list
 
+let string_of_move (x1, y1, x2, y2) =
+  (string_of_int x1) ^ " " ^ (string_of_int y1) ^ " " ^
+  (string_of_int x2) ^ " " ^ (string_of_int y2) ^ " -1 -1"
+
 let available_moves brd = 
   let stub = [(8,8,8,8)] in
 stub
