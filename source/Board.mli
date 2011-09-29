362b95cb407d3227fc000a549828e0542df85e26
Andrew Bunyea <abunyea@gmail.com>
2011-09-28 23:36:43 -0400

    Re-organized some stuff and updated the makefile

--- source/Board.mli
+++ source/Board.mli
@@ -1,5 +1,13 @@
-type space
-type board
+type space = P1 | P2 | Grey | Empty | Void
+type board = space array array
+type move = int * int * int * int
+type move_set = move list
 
 val print_board : board -> unit
 val read_input : unit -> board
+val copy_board : board -> board
+val do_move : board -> move -> board
+
+(* Prints a move for std out, with appropriate
+   -1s to not place a gray marble. *)
+val string_of_move : move -> string
--- source/main.ml
+++ source/main.ml
@@ -1,2 +1,8 @@
-let x = "4" in
-print_endline x
+open Board;;
+
+let brd = read_input() in
+let brd1 = do_move brd (9, 3, 8, 4) in
+
+((print_board brd);
+(print_newline());
+(print_board brd1));;
