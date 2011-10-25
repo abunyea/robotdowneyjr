2ff49a8bca4101c0369bf992d9e669ebeac96476
Andrew Bunyea <abunyea@gmail.com>
2011-10-25 19:02:28 -0400

    check_ending implemented

--- source/sixminuteab_bot.ml
+++ source/sixminuteab_bot.ml
@@ -4,6 +4,9 @@ open Manhattan_Bot
 
 (* minimum distance before switching to minimax *)
 let middle_distance = 8
+(* Number of their marbles that must have passed all of ours
+   before we switch to  *)
+let num_ending = 8
 
 let middle = ref false
 let ending = ref false
@@ -19,7 +22,20 @@ let check_middle state : unit =
     middle := abs (furthest_p1 - furthest_p2) <= middle_distance
 
 (* updates ending if an ending state is reached *)
-let check_ending state : unit = failwith "ahhhhh"
+let check_ending state : unit = 
+  if not !ending then 
+    let p1_list = build_piece_list_a state.board P1 in
+    let p2_list = build_piece_list_a state.board P2 in
+    let our_list = if state.player = Player1 then p1_list else p2_list in
+    let their_list = if state.player = Player1 then p2_list else p1_list in
+    let our_last = List.fold_left 
+      (fun a (_, x) -> (if state.player = Player1 then max else min) a x) 
+        19 our_list in
+    let num_behind = List.fold_left
+      (fun a (_, y) -> if (if state.player = Player1 then (>) else (<)) 
+         y our_last then a+1 else a)
+       0 their_list in
+    ending := num_behind >= num_ending
 
 let sixminuteab_bot state player =
   check_middle state;
