26c5ab02952f456d1315a8ecb52a3999df871945
Andrew Bunyea <abunyea@gmail.com>
2011-10-25 17:57:55 -0400

    basic structure for sixminuteab, calls manhattan bot

--- source/sixminuteab_bot.ml
+++ source/sixminuteab_bot.ml
@@ -1,4 +1,15 @@
 open Board
 open State
+open Manhattan_Bot
 
-let sixminuteab_bot state player = failwith "UNIMPLEMENTED"
+let middle = ref false
+let ending = ref false
+
+let sixminuteab_bot state player =
+  if not !middle then
+	(* manhattan *)
+    manhattan_bot state player
+  else if not !ending then
+    (* minimax *) failwith "no"
+  else
+    (* lookup table or search *) failwith "no"
---
+++ source/sixminuteab_bot.mli
@@ -0,0 +1,4 @@
+open Board
+open State
+
+val sixminuteab_bot : state -> player -> move
