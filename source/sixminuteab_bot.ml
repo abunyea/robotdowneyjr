fa75d540bccb6711a18fb3aaa3df30d7ef5476e2
Andrew Bunyea <abunyea@gmail.com>
2011-10-25 18:26:30 -0400

    check middle implemented

--- source/sixminuteab_bot.ml
+++ source/sixminuteab_bot.ml
@@ -2,12 +2,21 @@ open Board
 open State
 open Manhattan_Bot
 
+(* minimum distance before switching to minimax *)
+let middle_distance = 8
+
 let middle = ref false
 let ending = ref false
 
 (* checks distance between furthest marbles on each side,
  * updates middle *)
-let check_middle state : unit = failwith "unimplemented"
+let check_middle state : unit = 
+  if not !middle then 
+    let p1_list = build_piece_list_a state.board P1 in
+    let p2_list = build_piece_list_a state.board P2 in
+    let furthest_p1 = List.fold_left (fun a (_, x) -> min a x) 19 p1_list in
+    let furthest_p2 = List.fold_left (fun a (_, x) -> max a x) (-1) p2_list in
+    middle := abs (furthest_p1 - furthest_p2) <= middle_distance
 
 (* updates ending if an ending state is reached *)
 let check_ending state : unit = failwith "ahhhhh"
