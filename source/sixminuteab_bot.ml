82bd48a774dd078e2dbc3dc33f2d87a685070ab9
Andrew Bunyea <abunyea@gmail.com>
2011-10-25 18:03:16 -0400

    stubs for checking state transitions added

--- source/sixminuteab_bot.ml
+++ source/sixminuteab_bot.ml
@@ -5,11 +5,22 @@ open Manhattan_Bot
 let middle = ref false
 let ending = ref false
 
+(* checks distance between furthest marbles on each side,
+ * updates middle *)
+let check_middle state : unit = failwith "unimplemented"
+
+(* updates ending if an ending state is reached *)
+let check_ending state : unit = failwith "ahhhhh"
+
 let sixminuteab_bot state player =
+  check_middle state;
   if not !middle then
 	(* manhattan *)
     manhattan_bot state player
-  else if not !ending then
+
+  else (check_ending state;
+  if not !ending then
     (* minimax *) failwith "no"
+
   else
-    (* lookup table or search *) failwith "no"
+    (* lookup table or search *) failwith "no")
