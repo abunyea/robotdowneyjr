ba392e44aa0997686d7c2c0cacd6bd4607e1f3b9
Will <wkiffer@gmail.com>
2011-11-07 18:16:56 -0500

    adsfadfasfd

--- source/main.ml
+++ source/main.ml
@@ -1,7 +1,9 @@
 open Board;;
 open State;;
 open Random_bot;;
+open Minimax_bot;;
 open Baby_bot;;
+open Test_bot;;
 
 let run () = 
   let (first_player, initial_state) = read_initial_input () in
@@ -9,21 +11,21 @@ let run () =
   let rec run_helper player current =
     if current.player = player then (
 	  (* Our turn *)
-      prerr_endline "Our turn..";
+       prerr_endline "Our turn.."; 
       (* do and output the current move *)
-      let move = baby_bot current player in
+      let move = test_bot current player in
         print_endline (string_of_move move);
-        prerr_endline "Moved";
+        prerr_endline "Moved"; 
       (* get move status *)
       let status = int_of_string (read_line ()) in
         (match status with
          | 0 -> run_helper (toggle_player player) 
-                           (update_board current move)
+                           (update_board current (fst move))
          | _ -> prerr_endline ("Game over with code " ^ (string_of_int status)))
        )
     else (
 	  (* Their turn *)
-      prerr_endline "Their turn..";
+       prerr_endline "Their turn.."; 
       (* Read their move *)
       let next = read_opponent_input current in
         (match next.status with
