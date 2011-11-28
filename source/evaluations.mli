b4d3be65bd81d57101e21b934d518c86011e5739
Will <wkiffer@gmail.com>
2011-11-28 01:54:13 -0500

    adasd

--- source/evaluations.mli
+++ source/evaluations.mli
@@ -4,3 +4,7 @@ open State
 (* Evaluation based on how many turns it takes for bot 
  * to complete the game if the opponent doesn't move *)
 val evaluate1 : (state -> space -> move) -> space -> state -> float
+
+(* Evaluation based on evaluate1, except first set's all *)
+(* opponent's pieces to empty spaces *)
+val evaluate2 : (state -> space -> move) -> space -> state -> float
